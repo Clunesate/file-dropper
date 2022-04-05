307ceb62af21de68fc9afb877785cde0deb50f11
clunesate <slavikase@gmail.com>
2022-04-05 13:31:42 +0300

    change react version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {FileDropper} from "./lib";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <FileDropper/>
   </React.StrictMode>,
   document.getElementById('root')
 );
